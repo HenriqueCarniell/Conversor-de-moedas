12caec39a574d9631b8e031278461eee01892a71
Gustavo <gustavocarniel718@gmail.com>
2024-05-22 10:26:35 -0300

    fazendo o backend do conversor de moedas

--- server/server.js
+++ server/server.js
@@ -2,15 +2,30 @@ const express = require('express');
 const porta = 4000;
 const app = express();
 
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("Chamei o backend com sucesso");
 });
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
     res.send("çfjngaoifdsnbçoadsfnbkdnafbdab");
+});
+
+app.get("/moeda", (req, res) => {
+    const moedas = {
+        BRL: "real",
+        USD: "dolar",
+        EUR: "euro"
+    }
+
+
+    res.status(200).json(moedas);
+});
+
+app.get("/info", (req, res) => {
+    res.send("Moeda");
 });
 
 app.listen(porta, () => {
